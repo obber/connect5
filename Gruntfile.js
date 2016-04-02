51eee928d9815b8b919062ad971040e528f41eef
Kan Adachi <Kadachi1023@gmail.com>
2016-04-02 12:15:52 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    watch: {
+      files: ['**/*'],
+      tasks: ['concat', 'sass'],
+    },
+
+    concat: {
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/built.js',
+      },
+    },
+
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'dist/style.css': 'src/scss/style.scss'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default', ['concat', 'sass']);
+
+};
